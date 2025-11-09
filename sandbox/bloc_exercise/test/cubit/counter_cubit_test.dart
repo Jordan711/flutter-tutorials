1f8afffd83d7071317466c4b6cd4994ebe7eaea1
jordan711 <jordan.feng@hotmail.com>
2025-11-09 17:53:44 -0500

    Add tests for increment and decrement

--- sandbox/bloc_exercise/test/cubit/counter_cubit_test.dart
+++ sandbox/bloc_exercise/test/cubit/counter_cubit_test.dart
@@ -1,6 +1,7 @@
 // Run tests by `pub run test`
 
 import 'package:bloc_exercise/cubit/counter_cubit.dart';
+import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -17,11 +18,28 @@ void main() {
       counterCubit.close();
     });
 
-    test("The intitial state for the CounterCubit is CounterState(counterValue:0)", () {
-      // Two instances are each stored in different memory addresses
-      // Dart compares their location in memory so need to use Equatable
-      // It compares instances by value rather than their memory locations
-      expect(counterCubit.state, CounterState(counterValue: 0));
-    });
+    test(
+      "The intitial state for the CounterCubit is CounterState(counterValue:0)",
+      () {
+        // Two instances are each stored in different memory addresses
+        // Dart compares their location in memory so need to use Equatable
+        // It compares instances by value rather than their memory locations
+        expect(counterCubit.state, CounterState(counterValue: 0));
+      },
+    );
+
+    blocTest(
+      "The cubit should emit a CounterState(counterValue: 1, wasIncremented: true) when cubit.increment() function is called",
+      build: () => counterCubit,
+      act: (cubit) => cubit.increment(),
+      expect: () => [CounterState(counterValue: 1, wasIncremented: true)],
+    );
+
+    blocTest(
+      "The cubit should emit a CounterState(counterValue: -1, wasIncremented: false) when cubit.decrement() function is called",
+      build: () => counterCubit,
+      act: (cubit) => cubit.decrement(),
+      expect: () => [CounterState(counterValue: -1, wasIncremented: false)],
+    );
   });
 }
